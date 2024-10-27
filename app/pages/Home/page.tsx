d8969f299f3c179fa4da12854da579700fb631f2
mohamedabdelraouf <you@example.com>
2024-10-27 15:29:13 +0300

    start in homepage

---
+++ app/pages/Home/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../Header/page";
+
+const Home = () => {
+  return (
+    <div className="w-[90%] mx-auto">
+      <Header />
+      <div className="w-[90%] mx-auto mt-12">
+        <h1>Hello World</h1>
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
